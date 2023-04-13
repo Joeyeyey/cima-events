abfe2d56c391d6ced46575a3b51138029bf8faa9
Jeff Kwong <50752383+jleekwong@users.noreply.github.com>
2023-04-13 15:49:42 -0700

    Add delete modal

---
+++ client/src/components/EditEvent.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { API_URL } from '../api/config';
+import DatePicker from 'react-datepicker';
+import { EventDocument } from '../../../server/src/models/Event';
+import { Alert } from '@mui/material';
+
+enum Action {
+  Add = 'ADD',
+  Edit = 'EDIT',
+}
+
+interface EditEventProps {
+  onEditEvent: (event: EventDocument) => void;
+  event?: EventDocument;
+  action: Action;
+}
+
+export function EditEvent(props: EditEventProps) {
+  const [title, setTitle] = useState(props.event?.title ?? '');
+  const [startDate, setStartDate] = useState<Date | undefined>(props.event?.startDate ?? undefined);
+  const [endDate, setEndDate] = useState<Date | undefined>(props.event?.endDate ?? undefined);
+  const [location, setLocation] = useState(props.event?.location ?? '');
+
+  const handleSubmit = async (formEvent: React.FormEvent<HTMLFormElement>) => {
+    formEvent.preventDefault();
+
+    const confirmed = window.confirm(`Are you sure you want to edit "${title}"?`);
+    if (confirmed) {
+        try {
+            const response = await fetch(`${API_URL}/events/${props.event?._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ title, startDate, endDate, location }),
+            });
+            if (response.ok) {
+                const event = await response.json();
+                props.onEditEvent(event);
+            } else {
+                console.error('Failed to edit event:', response.status, response.statusText);
+            }
+        } catch (error) {
+            console.error('Failed to edit event:', error);
+        }
+    }
+
+  };
+
+  const handleTimeChange = (isStartTime: boolean, event: React.ChangeEvent<HTMLInputElement>) => {
+    const localDateTime = event.target.value;
+    const localDateTimeInMs = new Date(localDateTime).getTime();
+    const timeZoneOffsetInMs = new Date().getTimezoneOffset() * 60 * 1000;
+    const dateInLocalTimezone = new Date(localDateTimeInMs - timeZoneOffsetInMs);
+    isStartTime ? setStartDate(dateInLocalTimezone) : setEndDate(dateInLocalTimezone);
+  }
+
+
+  if (!props.event) {
+    return <Alert severity="error">Cannot edit an undefined event</Alert>
+  }
+
+  return (
+    <>
+    <h2>Edit Event</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" value={title} onChange={(event) => setTitle(event.target.value)} required />
+      </div>
+      <div>
+        <label htmlFor="startDate">Start date:</label>
+        <input
+          type="datetime-local"
+          id="startDate"
+          value={startDate ? startDate.toISOString().slice(0, -8) : ''}
+          onChange={(e) => {handleTimeChange(true, e)}}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="endDate">End date:</label>
+        <input
+          type="datetime-local"
+          id="endDate"
+          value={endDate ? endDate.toISOString().slice(0, -8) : ''}
+          onChange={(e) => {handleTimeChange(false, e)}}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="location">Location:</label>
+        <input type="text" id="location" value={location} onChange={(event) => setLocation(event.target.value)} required />
+      </div>
+      <button type="submit">Edit Event</button>
+    </form>
+</>
+  );
+}
--- client/src/components/EventCard.tsx
+++ client/src/components/EventCard.tsx
@@ -1,10 +1,11 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, Tooltip, Typography } from '@mui/material';
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Tooltip, Typography } from '@mui/material';
 import DeleteForeverTwoToneIcon from '@mui/icons-material/DeleteForeverTwoTone';
 import EditIcon from '@mui/icons-material/Edit';
 // import { Delete } from "@mui/icons-material";
 import { EventDocument } from '../../../server/src/models/Event';
 import { API_URL } from '../api/config';
 import { render } from 'react-dom';
+import { useState } from 'react';
 
 interface Props {
   event: EventDocument;
@@ -14,10 +15,11 @@ interface Props {
 
 const EventCard = ({ event, handleDeleteCard, handleEditCard }: Props) => {
   if (!event) {
-    return null;
+    return <></>;
   }
 
   const { title, startDate, endDate, location } = event;
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);;
 
   function renderTitleAndLocation(title: string, location: string): string {
     return `${title ?? ''} @ ${location ?? ''}`;
@@ -49,24 +51,69 @@ const EventCard = ({ event, handleDeleteCard, handleEditCard }: Props) => {
           </IconButton>
         </Tooltip>
 
-
         <Tooltip title="Delete" followCursor>
-          <IconButton onClick={() => { handleDeleteCard(event) }}>
+          <IconButton
+            onClick={() => {
+              setIsDialogOpen(true);
+              // handleDeleteCard(event);
+            }}>
             <DeleteForeverTwoToneIcon />
           </IconButton>
         </Tooltip>
+
+        <Dialog
+          open={isDialogOpen}
+          onClose={() => {setIsDialogOpen(false)}}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {`Delete ${event.title}?`}
+          </DialogTitle>
+          <DialogActions>
+            <Button onClick={() => {setIsDialogOpen(false)}}>No</Button>
+            <Button onClick={() => {handleDeleteCard(event)}} autoFocus>
+              Yes
+            </Button>
+          </DialogActions>
+        </Dialog>
       </>
     )
   };
 
   return (
-    <CardActionArea>
-      <Card>
+    // <CardActionArea>
+    //   <Card>
+    //     <CardMedia
+    //       component="img"
+    //       height="140"
+    //       image="https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    //       alt="green iguana"
+    //     />
+    //     <CardContent className='cardContent'>
+    //       <Typography variant="h5" component="h2">
+    //         {renderTitleAndLocation(title, location)}
+    //       </Typography>
+    //       <Typography variant="h5" component="h2">
+    //         {renderDate(true, startDate)}
+    //       </Typography>
+    //       <Typography variant="h5" component="h2">
+    //         {renderDate(false, endDate)}
+    //       </Typography>
+    //     </CardContent>
+    //     <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+    //       {renderButtons()}
+    //     </CardActions>
+    //   </Card>
+    // </CardActionArea>
+    // <Card sx={{ maxWidth: 345 }}>
+    <Card>
+      <CardActionArea>
         <CardMedia
           component="img"
           height="140"
           image="https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt="green iguana"
+          alt="event photo"
         />
         <CardContent className='cardContent'>
           <Typography variant="h5" component="h2">
@@ -79,11 +126,16 @@ const EventCard = ({ event, handleDeleteCard, handleEditCard }: Props) => {
             {renderDate(false, endDate)}
           </Typography>
         </CardContent>
+      </CardActionArea>
+      <CardActions>
         <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
           {renderButtons()}
         </CardActions>
-      </Card>
-    </CardActionArea>
+        {/* <Button size="small" color="primary">
+          Share
+        </Button> */}
+      </CardActions>
+    </Card>
   );
 };
 
